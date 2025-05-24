e13762bffd38ddca92624b46901f998a0d928817
Ilya Lednev <vl.lednev16082005@gmail.com>
2025-05-24 12:53:53 +1000

    Merge

--- password.cpp
+++ password.cpp
@@ -1,13 +1,26 @@
+/**
+ * @file password.cpp
+ * @brief Генератор случайных паролей
+ * @author Леднев Илья Алексеевич
+ * @date 2023
+ */
 #include <iostream>
 #include <string>
 #include <cstdlib>
 #include <ctime>
-
+#include <clocale>
+//проверка CI/CD
 using namespace std;
 
-/*ИЗМЕНЕНИЯ*/
+/*ИЗМЕНЕНИЕ*/
 
-
+/**
+ * @brief Генерирует случайный пароль
+ * @param length Длина пароля
+ * @param useDigits Использовать цифры
+ * @param useSymbols Использовать спецсимволы
+ * @return Сгенерированный пароль
+ */
 string generatePassword(int length, bool useDigits, bool useSymbols) {
     const string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const string digits = "0123456789";
@@ -25,16 +38,25 @@ string generatePassword(int length, bool useDigits, bool useSymbols) {
     return password;
 }
 
+/**
+ * @brief Основная функция программы
+ * @return Код завершения (0 - успех, 1 - ошибка)
+ */
+
 int main() {
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    
     int length;
     bool digits, symbols;
 
+    cout << "Длина пароля: "; 
+    cin >> length;
+    
     if (length < 6) {
         cout << "Пароль должен быть не короче 6 символов!" << endl;
         return 1;
     }
-    cout << "Длина пароля: ";
-    cin >> length;
+    
     cout << "Цифры (1/0): ";
     cin >> digits;
     cout << "Спецсимволы (1/0): ";
